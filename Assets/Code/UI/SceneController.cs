f5697cfbfe68719b216f252b0b4fded6c0587319
Mateo Cayo <164943255+mateuscayus@users.noreply.github.com>
2025-06-24 13:59:07 -0500

    transiciones implementadas, los botones In game no funcionan :v

--- Assets/Code/UI/SceneController.cs
+++ Assets/Code/UI/SceneController.cs
@@ -1,18 +1,84 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
-public class SceneController : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-    public string sceneName;
-    void Start()
+    [SerializeField] private string escenaNombre;
+    [SerializeField] private bool usarFade = false;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeOutDuration;
+    [SerializeField] private float fadeInDuration;
+
+    private void Start()
     {
-
-
-
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            Color color = fadeImage.color;
+            color.a = 1; // Comenzamos con opacidad completa
+            fadeImage.color = color;
+            StartCoroutine(FadeOutAtStart());
+        }
     }
 
-public void SceneChanger()
+    private IEnumerator FadeOutAtStart()
     {
-        SceneManager.LoadScene(sceneName);
+        float timer = 0;
+        Color color = fadeImage.color;
+
+        while (timer < fadeOutDuration)
+        {
+            timer += Time.unscaledDeltaTime;
+            color.a = Mathf.Lerp(1, 0, timer / fadeOutDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(false); // Oculta la imagen luego del fade out
     }
+
+    public void ChangeScene()
+    {
+        if (usarFade && fadeImage != null)
+        {
+            StartCoroutine(FadeInAndChangeScene());
+        }
+        else
+        {
+            SceneManager.LoadScene(escenaNombre);
+        }
+    }
+
+    private IEnumerator FadeInAndChangeScene()
+    {
+        fadeImage.gameObject.SetActive(true);
+        float timer = 0;
+        Color color = fadeImage.color;
+
+        while (timer < fadeInDuration)
+        {
+            timer += Time.unscaledDeltaTime;
+            color.a = Mathf.Lerp(0, 1, timer / fadeInDuration);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(escenaNombre);
+    }
+
+    #region Editor Support Methods
+
+    public string GetSceneName()
+    {
+        return escenaNombre;
+    }
+
+    public void SetSceneName(string sceneName)
+    {
+        escenaNombre = sceneName;
+    }
+
+    #endregion
 }
