440314a2a3df9f86da3864b9ca8122892c6dc043
Alonso Pérez <edalopp@hotmail.com>
2025-07-01 20:39:38 -0500

    Revert "curve"
    This reverts commit 4b79abb5acc0128602a8c2248d31dd1ad4309cd6.

--- Assets/Code/PointsManager.cs
+++ Assets/Code/PointsManager.cs
@@ -8,57 +8,22 @@ public class PointsManager : MonoBehaviour
     public TMP_Text pointsText;
     private int points;
 
-    private Transform playerTransform;
-    private CurvePoint curveReference;
-
-    private float nearCurveTimer = 0f;
-    [SerializeField] private float checkInterval = 0.1f;
-    private float checkCooldown = 0f;
-
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Start()
     {
         points = 0;
         UpdatePointsText();
-    }
-
-    private void Update()
-    {
-        checkCooldown -= Time.deltaTime;
-        if (checkCooldown <= 0f)
-        {
-            checkCooldown = checkInterval;
-
-            if (curveReference != null && playerTransform != null)
-            {
-                float playerY = playerTransform.position.y;
-                float playerX = playerTransform.position.x;
-
-                float curveY = curveReference.GetCurveYAtCurrentTimeAndX(playerX);
-                float distanceY = Mathf.Abs(playerY - curveY);
-
-                if (distanceY < 1f)
-                {
-                    nearCurveTimer += checkInterval;
-
-                    if (nearCurveTimer >= 1f)
-                    {
-                        int seconds = Mathf.FloorToInt(nearCurveTimer);
-                        AddPoint(seconds * 2);
-                        nearCurveTimer -= seconds;
-                    }
-                }
-                else
-                {
-                    nearCurveTimer = 0f;
-                }
-            }
-        }
     }
 
     public void AddPoint(int amount)
